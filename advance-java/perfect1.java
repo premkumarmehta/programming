7752e0b067b397c04ff76daa77a6cdbaafee4476
premkumartech <premkumartech123@gmail.com>
2025-05-04 15:20:16 +0530

    generate perfect value

--- advance-java/perfect1.java
+++ advance-java/perfect1.java
@@ -1,25 +1,51 @@
-import java.util.Scanner;
+
+// check perfect value or not
+
+// import java.util.Scanner;
+
+// public class perfect1 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter a value: ");
+//         int n = sc.nextInt();
+
+//         int s = 0;
+//         int i = 1;
+//         while (i<=n/2) {
+//             if(n%i==0){
+//                 s = s+i;
+//             }
+//             i++;
+//         }
+//         if(n==s)
+//         System.out.println("Perfect Value");
+//         else
+//         System.out.println("Not Perfect Value");
+
+//         sc.close();
+//     }
+// }
+
+
+
+// Generate perfect value from 1 to 1000.
 
 public class perfect1 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a value: ");
-        int n = sc.nextInt();
-
-        int s = 0;
         int i = 1;
-        while (i<=n/2) {
-            if(n%i==0){
-                s = s+i;
+        while (i<=1000) {
+            int n = i, j=1, s=0;
+            while (j<n/2) {
+                if(n%j==0){
+                    s = s+j;
+                }
+                j++;
             }
+            if(s==n)
+            System.out.println(s);
             i++;
         }
-        if(n==s)
-        System.out.println("Perfect Value");
-        else
-        System.out.println("Not Perfect Value");
-
-        sc.close();
     }
 }
